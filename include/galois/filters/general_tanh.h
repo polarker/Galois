8b588db621dcd951c3e53279c0ae49dfdaf0c597
Cheng Wang <chengwang0511@gmail.com>
2015-11-11 16:30:05 +0100

    update tanh

---
+++ include/galois/filters/general_tanh.h
@@ -0,0 +1,29 @@
+#ifndef _GALOIS_TANH_H_
+#define _GALOIS_TANH_H_
+
+#include "galois/base.h"
+
+namespace gs {
+    
+    template<typename T>
+    class GeneralTanh : public BFilter<T> {
+    private:
+        SP_Signal<T> in_signal = nullptr;
+        SP_Signal<T> out_signal = nullptr;
+    public:
+        Tanh() {}
+        Tanh(const Tanh&) = delete;
+        Tanh& operator=(const Tanh&) = delete;
+        
+        SP_Filter<T> share() override;
+        
+        void install_signals(const vector<SP_Signal<T>> &in_signals, const vector<SP_Signal<T>> &out_signals) override;
+        void set_dims(int batch_size) override;
+
+        void forward() override;
+        void backward() override;
+    };
+    
+}
+
+#endif
--- src/filters/general_tanh.cc
+++ src/filters/general_tanh.cc
@@ -4,14 +4,14 @@
 namespace gs {
     
     template<typename T>
-    SP_Filter<T> Tanh<T>::share() {
+    SP_Filter<T> GeneralTanh<T>::share() {
         CHECK(in_signal == nullptr, "in signal should not be set");
         CHECK(out_signal == nullptr, "out signal should not be set");
-        return make_shared<Tanh<T>>();
+        return make_shared<GeneralTanh<T>>();
     }
     
     template<typename T>
-    void Tanh<T>::install_signals(const vector<SP_Signal<T>> &in_signals, const vector<SP_Signal<T>> &out_signals) {
+    void GeneralTanh<T>::install_signals(const vector<SP_Signal<T>> &in_signals, const vector<SP_Signal<T>> &out_signals) {
         CHECK(in_signal == nullptr, "in signal should not be initialized");
         CHECK(out_signal == nullptr, "out signal should not be initialized");
         CHECK(in_signals.size() == 1, "need only 1 in signal");
@@ -22,7 +22,7 @@ namespace gs {
     }
     
     template<typename T>
-    void Tanh<T>::set_dims(int batch_size) {
+    void GeneralTanh<T>::set_dims(int batch_size) {
         CHECK(!in_signal->empty(), "in signal should be initialized");
         auto in_dims = in_signal->get_data_dims();
         if (out_signal->empty()) {
@@ -33,21 +33,20 @@ namespace gs {
     }
         
     template<typename T>
-    void Tanh<T>::forward() {
+    void GeneralTanh<T>::forward() {
         auto in_data = in_signal->get_data();
         auto out_data = out_signal->get_data();
-        CHECK(out_data->opaque(), "This Tanh could not work in parallel with the other filters, please use GeneralTanh instead");
         
         MAP(out_data, [](T x){ return tanh(x); }, in_data);
     }
     
     template<typename T>
-    void Tanh<T>::backward() {
+    void GeneralTanh<T>::backward() {
+        auto in_data = in_signal->get_data();
         auto in_grad = in_signal->get_grad();
-        auto out_data = out_signal->get_data();
         auto out_grad = out_signal->get_grad();
         
-        MAP(in_grad, [](T dy, T y){return dy*(1-y*y);}, out_grad, out_data);
+        MAP(in_grad, [](T dy, T x){return dy*(1-tanh(x)*tanh(x));}, out_grad, in_data);
     }
     
     template class Tanh<float>;
