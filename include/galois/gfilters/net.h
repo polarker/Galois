e62332e3eeab40eba1d17f1503bbece532a1877a
Cheng Wang <chengwang0511@gmail.com>
2015-11-12 15:48:18 +0100

    refactor net

--- include/galois/gfilters/net.h
+++ include/galois/gfilters/net.h
@@ -2,6 +2,7 @@
 #define _GALOIS_NET_H_
 
 #include "galois/base.h"
+#include "galois/gfilters/netbase.h"
 #include <string>
 #include <vector>
 #include <set>
@@ -13,62 +14,30 @@ namespace gs
 {
     
     template<typename T>
-    class Net : public GFilter<T>
+    class Net : public NetBase<T>
     {
     private:
-        vector<tuple<const vector<string>, const vector<string>, SP_Filter<T>>> links;
-        set<SP_PFilter<T>> pfilters;
+        map<string, vector<tuple<string, int>>> fp_graph = {};
+        map<string, vector<tuple<string, int>>> bp_graph = {};
         
-        map<string, vector<tuple<string, int>>> fp_graph;
-        map<string, vector<tuple<string, int>>> bp_graph;
-        
-        map<string, SP_Signal<T>> inner_signals;
-        
-        vector<string> input_ids;
-        vector<string> output_ids;
-        
-        vector<int> fp_order;
-        vector<SP_Filter<T>> fp_filters;
-        vector<int> bp_order;
-        vector<SP_Filter<T>> bp_filters;
+    private:
+        void _set_fp_order(const string, vector<int>&);
         
     public:
-        Net();
+        Net() {}
         Net(const Net& other) = delete;
         Net& operator=(const Net&) = delete;
         
-        void add_link(const vector<string>&, const vector<string>&, SP_Filter<T>);
-        void add_link(const initializer_list<string>, const initializer_list<string>, SP_Filter<T>);
-        void add_link(const initializer_list<string>, const string, SP_Filter<T>);
-        void add_link(const string, const initializer_list<string>, SP_Filter<T>);
-        void add_link(const string, const string, SP_Filter<T>);
-        void _remove_signal(string);
+        void add_link(const vector<string>&, const vector<string>&, SP_Filter<T>) override;
         void set_input_ids(const string);
         void set_input_ids(const initializer_list<string>);
         void set_input_ids(const vector<string>&);
         void set_output_ids(const string);
         void set_output_ids(const initializer_list<string>);
         void set_output_ids(const vector<string>&);
-        void _set_fp_order(const string);
-        void _set_bp_order(const string);
         void set_p_order();
         
-        // in order to share a net, methods above should be called and methods below should not be called
         SP_Filter<T> share() override;
-        set<SP_PFilter<T>> get_pfilters() override;
-        
-        SP_Signal<T> _get_signal(string id,
-                                 const vector<SP_Signal<T>> in_signals,
-                                 const vector<SP_Signal<T>> out_signals);
-        vector<SP_Signal<T>> _get_signal(vector<string> id,
-                                         const vector<SP_Signal<T>> in_signals,
-                                         const vector<SP_Signal<T>> out_signals);
-        void install_signals(const vector<SP_Signal<T>> &in_signals, const vector<SP_Signal<T>> &out_signals) override;
-        void set_dims(int batch_size) override;
-        void reopaque() override;
-        
-        void forward() override;
-        void backward() override;
     };
 
 }
--- src/gfilters/net.cc
+++ src/gfilters/net.cc
@@ -5,32 +5,11 @@
 namespace gs {
     
     template<typename T>
-    Net<T>::Net() : links{}
-            , pfilters{}
-            , fp_graph{}
-            , bp_graph{}
-            , inner_signals{}
-            , input_ids{}
-            , output_ids{}
-            , fp_order{}
-            , bp_order{}{
-    }
-    
-    template<typename T>
     void Net<T>::add_link(const vector<string> &ins, const vector<string> &outs, SP_Filter<T> filter){
         // add to links
-        auto idx = links.size();
-        links.push_back(tuple<const vector<string>,const vector<string>,SP_Filter<T>>
+        auto idx = this->links.size();
+        this->links.push_back(tuple<const vector<string>,const vector<string>,SP_Filter<T>>
                         (vector<string>{ins}, vector<string>{outs}, filter));
-        
-        // add to pfilters
-        if (auto p = dynamic_pointer_cast<PFilter<T>>(filter)) {
-            pfilters.insert(p);
-        }
-        if (auto g = dynamic_pointer_cast<GFilter<T>>(filter)) {
-            auto s = g->get_pfilters();
-            pfilters.insert(s.begin(), s.end());
-        }
         
         // add to graph
         for (auto s : ins) {
@@ -52,41 +31,14 @@ namespace gs {
         
         // initial signals
         for (auto s : ins) {
-            if (!Contains(input_ids, s) && (inner_signals.count(s) == 0)) {
-                inner_signals[s] = make_shared<Signal<T>>(InnerSignal);
+            if (!Contains(this->input_ids, s) && (this->inner_signals.count(s) == 0)) {
+                this->inner_signals[s] = make_shared<Signal<T>>(InnerSignal);
             }
         }
         for (auto s : outs) {
-            if (!Contains(output_ids, s) && (inner_signals.count(s) == 0)) {
-                inner_signals[s] = make_shared<Signal<T>>(InnerSignal);
+            if (!Contains(this->output_ids, s) && (this->inner_signals.count(s) == 0)) {
+                this->inner_signals[s] = make_shared<Signal<T>>(InnerSignal);
             }
-        }
-    }
-    
-    template<typename T>
-    void Net<T>::add_link(const initializer_list<string> ins, const initializer_list<string> outs, SP_Filter<T> filter){
-        add_link(vector<string>(ins), vector<string>(outs), filter);
-    }
-    
-    template<typename T>
-    void Net<T>::add_link(const initializer_list<string> ins, const string outs, SP_Filter<T> filter){
-        add_link(ins, {outs}, filter);
-    }
-    
-    template<typename T>
-    void Net<T>::add_link(const string ins, const initializer_list<string> outs, SP_Filter<T> filter){
-        add_link({ins}, outs, filter);
-    }
-    
-    template<typename T>
-    void Net<T>::add_link(const string ins, const string outs, SP_Filter<T> filter){
-        add_link({ins}, {outs}, filter);
-    }
-    
-    template<typename T>
-    void Net<T>::_remove_signal(string id) {
-        if (inner_signals.count(id) > 0) {
-            inner_signals.erase(id);
         }
     }
     
@@ -102,10 +54,10 @@ namespace gs {
     
     template<typename T>
     void Net<T>::set_input_ids(const vector<string>& ids) {
-        CHECK(input_ids.empty(), "input_ids should not be set");
-        input_ids.insert(input_ids.end(), ids.begin(), ids.end());
+        CHECK(this->input_ids.empty(), "input_ids should not be set");
+        this->input_ids.insert(this->input_ids.end(), ids.begin(), ids.end());
         for (auto id : ids) {
-            _remove_signal(id);
+            this->_remove_signal(id);
         }
     }
     
@@ -121,18 +73,16 @@ namespace gs {
     
     template<typename T>
     void Net<T>::set_output_ids(const vector<string>& ids) {
-        CHECK(output_ids.empty(), "output_ids should not be set");
-        output_ids.insert(output_ids.end(), ids.begin(), ids.end());
+        CHECK(this->output_ids.empty(), "output_ids should not be set");
+        this->output_ids.insert(this->output_ids.end(), ids.begin(), ids.end());
         for (auto id : ids) {
-            _remove_signal(id);
+            this->_remove_signal(id);
         }
     }
     
     template<typename T>
-    void Net<T>::_set_fp_order(string out_id) {
-        // input_ids should be non-empty
-        CHECK(!input_ids.empty(), "input ids should have been set");
-        if (Contains(input_ids, out_id)) {
+    void Net<T>::_set_fp_order(string out_id, vector<int> &fp_order) {
+        if (Contains(this->input_ids, out_id)) {
             if (bp_graph.count(out_id) == 0) {
                 return;
             }
@@ -144,7 +94,7 @@ namespace gs {
             string in_id = get<0>(t);
             int link_idx = get<1>(t);
             if (!Contains(fp_order, link_idx)) {
-                _set_fp_order(in_id);
+                _set_fp_order(in_id, fp_order);
             }
         }
         for (auto t : bp_graph[out_id]) {
@@ -156,57 +106,27 @@ namespace gs {
     }
     
     template<typename T>
-    void Net<T>::_set_bp_order(string in_id) {
-        CHECK(!output_ids.empty(), "output ids should have been set");
-        if (Contains(output_ids, in_id)) {
-            if (fp_graph.count(in_id) == 0) {
-                return;
-            }
-        } else {
-            CHECK(fp_graph.count(in_id) == 1, "in_id should be in the keys of fp_graph");
-        }
-        for (auto t : fp_graph[in_id]) {
-            string out_id = get<0>(t);
-            int link_idx = get<1>(t);
-            if (!Contains(bp_order, link_idx)) {
-                _set_bp_order(out_id);
-            }
-        }
-        for (auto t : fp_graph[in_id]) {
-            int link_idx = get<1>(t);
-            if (!Contains(bp_order, link_idx)) {
-                bp_order.push_back(link_idx);
-            }
-        }
-    }
-    
-    template<typename T>
     void Net<T>::set_p_order() {
-        CHECK(fp_order.empty(), "fp order should not be set");
-        CHECK(bp_order.empty(), "bp order should not be set");
-        CHECK(fp_filters.empty(), "fp filters should not be set");
-        CHECK(bp_filters.empty(), "bp filters should not be set");
-        for (auto out_id : output_ids) {
-            _set_fp_order(out_id);
-        }
-        for (auto in_id : input_ids) {
-            _set_bp_order(in_id);
+        CHECK(!this->fixed, "network should not be fixed");
+        CHECK(!this->input_ids.empty(), "input ids should have been set");
+        CHECK(!this->output_ids.empty(), "output ids should have been set");
+        CHECK(this->fp_filters.empty(), "fp filters should not be set");
+        this->fixed = true;
+        
+        vector<int> fp_order{};
+        for (auto out_id : this->output_ids) {
+            _set_fp_order(out_id, fp_order);
         }
         for (auto link_idx : fp_order) {
-            auto t = links[link_idx];
+            auto t = this->links[link_idx];
             auto filter = get<2>(t);
-            fp_filters.push_back(filter);
-        }
-        for (auto link_idx : bp_order) {
-            auto t = links[link_idx];
-            auto filter = get<2>(t);
-            bp_filters.push_back(filter);
+            this->fp_filters.push_back(filter);
         }
     }
     
     template<typename T>
     SP_Filter<T> Net<T>::share() {
-        CHECK(!fp_order.empty() && !bp_order.empty(), "these order should have been set and the net is fixed");
+        CHECK(this->fixed, "the network should be fixed");
         auto res = make_shared<Net<T>>();
         for (auto t : this->links) {
             auto ins = get<0>(t);
@@ -219,93 +139,9 @@ namespace gs {
         res->set_output_ids(this->output_ids);
         res->set_p_order();
         
-        CHECK(res->fp_graph == this->fp_graph &&
-              res->bp_graph == this->bp_graph &&
-              res->fp_order == this->fp_order &&
-              res->bp_order == this->bp_order, "these should be equal");
+        CHECK(res->fp_filters == this->fp_filters, "these should be equal");
         
         return res;
-    }
-    
-    template<typename T>
-    set<SP_PFilter<T>> Net<T>::get_pfilters() {
-        CHECK(!fp_order.empty() && !bp_order.empty(), "these order should have been set and the net is fixed");
-        return pfilters;
-    }
-    
-    template<typename T>
-    SP_Signal<T> Net<T>::_get_signal(string id,
-                                        const vector<SP_Signal<T>> in_signals,
-                                        const vector<SP_Signal<T>> out_signals) {
-        auto in_idx = find(input_ids.begin(), input_ids.end(), id);
-        if (in_idx != input_ids.end()) {
-            auto idx = in_idx - input_ids.begin();
-            return in_signals[idx];
-        }
-        auto out_idx = find(output_ids.begin(), output_ids.end(), id);
-        if (out_idx != output_ids.end()) {
-            auto idx = out_idx - output_ids.begin();
-            return out_signals[idx];
-        }
-        return inner_signals[id];
-    }
-    
-    template<typename T>
-    vector<SP_Signal<T>> Net<T>::_get_signal(vector<string> ids,
-                                                const vector<SP_Signal<T>> in_signals,
-                                                const vector<SP_Signal<T>> out_signals) {
-        vector<SP_Signal<T>> res{};
-        for (auto id : ids) {
-            res.push_back(_get_signal(id, in_signals, out_signals));
-        }
-        return res;
-    }
-    
-    template<typename T>
-    void Net<T>::install_signals(const vector<SP_Signal<T>> &in_signals, const vector<SP_Signal<T>> &out_signals) {
-        for (auto t : links) {
-            auto ins = get<0>(t);
-            auto outs = get<1>(t);
-            auto filter = get<2>(t);
-            filter->install_signals(_get_signal(ins, in_signals, out_signals),
-                                    _get_signal(outs, in_signals, out_signals));
-        }
-    }
-    
-    template<typename T>
-    void Net<T>::set_dims(int batch_size) {
-        CHECK(!fp_order.empty(), "fp order should have been set");
-        CHECK(!bp_order.empty(), "bp order should have been set");
-        for (auto filter : fp_filters) {
-            filter->set_dims(batch_size);
-        }
-    }
-
-    template<typename T>
-    void Net<T>::reopaque() {
-        for (auto &kv : inner_signals) {
-            auto signal = kv.second;
-            signal->reopaque();
-        }
-        for (auto filter : fp_filters) {
-            filter->reopaque();
-        }
-    }
-    
-    template<typename T>
-    void Net<T>::forward() {
-        CHECK(!fp_filters.empty(), "fp filters should have been set");
-        for (auto filter : fp_filters) {
-            filter->forward();
-        }
-    }
-
-    template<typename T>
-    void Net<T>::backward() {
-        CHECK(!bp_filters.empty(), "bp filters should have been set");
-        for (auto filter : bp_filters) {
-            filter->backward();
-        }
     }
     
     template class Net<float>;
